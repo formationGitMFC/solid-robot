abdd6c648d231bf4fc5bf297e1c8f57ed8811d3c
Sylvain Febvre <sylvain.febvre@gmail.com>
2021-06-25 15:06:30 +0100

    Not for nubz

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const name = prompt(`Vous etes ?`);
 
 alert(`Hello ${name} !`);
+
+for(let i = 0; i < 10; i++) {
+    const rep = confirm(`Es tu heureux ?`);
+    alert(!rep ? `Non ? tu es sur ?` : `C'est bien ${name}`);
+}
